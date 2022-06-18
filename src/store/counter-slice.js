5b700c7d366472b2e11475cd13e88918f5400a74
ilkyazar <ilkyaz.arabaci@gmail.com>
2022-06-18 22:22:19 +0300

    put slices into different files

---
+++ src/store/counter-slice.js
@@ -0,0 +1,33 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialCounterState = { counter: 0, showCounter: true };
+
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState: initialCounterState,
+  reducers: {
+    increment(state) {
+      // We don't actually manipulating the state.
+      // Redux uses imgur package,
+      // which detect code like this
+      // and automatically clone the existing state,
+      // create a new state object,
+      // keep all the state we're not editing
+      // and overwrite the state we are editing
+      // in an immutable way.
+      // So, this is ok, here.
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.payload;
+    },
+    toggle(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
+
+export default counterSlice;
--- src/store/index.js
+++ src/store/index.js
@@ -1,56 +1,6 @@
 import { createSlice, configureStore } from '@reduxjs/toolkit';
-
-export const INCREMENT = 'increment';
-export const DECREMENT = 'decrement';
-export const INCREASE = 'increase';
-export const TOGGLE = 'toggle';
-
-const initialCounterState = { counter: 0, showCounter: true };
-
-const counterSlice = createSlice({
-  name: 'counter',
-  initialState: initialCounterState,
-  reducers: {
-    increment(state) {
-      // We don't actually manipulating the state.
-      // Redux uses imgur package,
-      // which detect code like this
-      // and automatically clone the existing state,
-      // create a new state object,
-      // keep all the state we're not editing
-      // and overwrite the state we are editing
-      // in an immutable way.
-      // So, this is ok, here.
-      state.counter++;
-    },
-    decrement(state) {
-      state.counter--;
-    },
-    increase(state, action) {
-      state.counter = state.counter + action.payload;
-    },
-    toggle(state) {
-      state.showCounter = !state.showCounter;
-    },
-  },
-});
-
-const initialAuthState = {
-  isAuthenticated: false,
-};
-
-const authSlice = createSlice({
-  name: 'auth',
-  initialState: initialAuthState,
-  reducers: {
-    login(state) {
-      state.isAuthenticated = true;
-    },
-    logout(state) {
-      state.isAuthenticated = false;
-    },
-  },
-});
+import counterSlice from './counter-slice';
+import authSlice from './auth-slice';
 
 // In case of multiple reducers, they can be merged in configureStore
 const store = configureStore({
