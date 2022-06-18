0b70b0fc9f2dde36514dfe4cded2ff264bb6a3d1
ilkyazar <ilkyaz.arabaci@gmail.com>
2022-06-18 21:44:26 +0300

    create slice with redux toolkit

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
 import { createStore } from 'redux';
+import { createSlice } from '@reduxjs/toolkit';
 
 export const INCREMENT = 'increment';
 export const DECREMENT = 'decrement';
@@ -6,6 +7,34 @@ export const INCREASE = 'increase';
 export const TOGGLE = 'toggle';
 
 const initialState = { counter: 0, showCounter: true };
+
+createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    increment(state) {
+      // We don't actually manipulating the state.
+      // Redux uses imgur package,
+      // which detect code like this
+      // and automatically clone the existing state,
+      // create a new state object,
+      // keep all the state we're not editing
+      // and overwrite the state we are editing
+      // in an immutable way.
+      // So, this is ok, here.
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.amount;
+    },
+    toggle(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
 
 const counterReducer = (state = initialState, action) => {
   if (action.type === INCREMENT) {
